6a2a8bbef65d4552b7c4d3d1c90c25a93ee15588
Benedict Juretko <benedict.juretko@gmail.com>
2019-05-02 00:24:22 +0200

    Add some comments

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 set -e
 
+# This entrypoint script just sets the correct permissions on the
+# shared volume and executes CMD with the less-privileged user "nobody"
+# See https://github.com/moby/moby/issues/22258 for more information
+# on the issue with permissions on shared volume.
+
 # get uid of the shared volume which is the uid from the host directory
 UID=${UID:-$(stat -c '%u' ${BAIKAL_DATA})}
 
+# if UID is already remapped or we are root (like on macOS) do nothing
+# if not set uid of "nobody" to the host's uid of the shared volume
 if [ "$(id -u nobody)" -ne "${UID}" -a "${UID}" -ge "0" ]; then
   # we will install usermod here to change the uid of the user "nobody"
   # see https://github.com/chrootLogin/docker-nextcloud/issues/3
